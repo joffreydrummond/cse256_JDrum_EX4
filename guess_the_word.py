523f9366caa465e1e57850c088cca87e8326a06c
Joffrey <jaydru@gmail.com>
2025-11-14 10:40:05 -0700

    Added initialize game to start the game

--- guess_the_word.py
+++ guess_the_word.py
@@ -5,3 +5,12 @@ WORDS = ["coffee", "backpack", "notebook", "python", "sunshine", "weekend"]
 
 def choose_word():
     return random.choice(WORDS)
+
+def initialize_game(word):
+    # Track the letters guessed so far and incorrect guesses
+    return {
+        "word": word,
+        "display": ["_"] * len(word),
+        "attempts_left": 6,
+        "wrong_letters": set()
+    }
