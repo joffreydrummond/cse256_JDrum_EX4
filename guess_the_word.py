b7143713114231d9062cf38c08e04fb8d6e3f756
Joffrey <jaydru@gmail.com>
2025-11-14 10:28:17 -0700

    Added the list of words and choose_word() method

---
+++ guess_the_word.py
@@ -0,0 +1,7 @@
+import random
+
+# Simple set of words for the game
+WORDS = ["coffee", "backpack", "notebook", "python", "sunshine", "weekend"]
+
+def choose_word():
+    return random.choice(WORDS)
