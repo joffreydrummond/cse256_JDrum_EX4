6a62de9ae12b997d5a19123fd94cb9ffc8fc5c1f
Joffrey <jaydru@gmail.com>
2025-11-16 08:55:32 -0700

    Added process guess to check letters if they are in the word

--- guess_the_word.py
+++ guess_the_word.py
@@ -14,3 +14,22 @@ def initialize_game(word):
         "attempts_left": 6,
         "wrong_letters": set()
     }
+
+def process_guess(game, letter):
+    letter = letter.lower()
+
+    # If the player already guessed this letter, do nothing
+    if letter in game["wrong_letters"] or letter in game["display"]:
+        return False
+
+    # If the letter is in the word, show it
+    if letter in game["word"]:
+        for i, char in enumerate(game["word"]):
+            if char == letter:
+                game["display"][i] = letter
+        return True
+    else:
+        # Wrong guess
+        game["wrong_letters"].add(letter)
+        game["attempts_left"] -= 1
+        return False
