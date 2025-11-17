fc64eda0e3cf1a320746016f4a0819c483e2c584
Joffrey <jaydru@gmail.com>
2025-11-16 21:06:07 -0700

    Added test for the correct letter in process guess method

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -1,6 +1,14 @@
 from guess_the_word import choose_word, WORDS, process_guess, initialize_game
 
 def test_word_selection():
-    # make sure the chosen word is actually from the list
+    # make sure the word picked is actually from the list
     word = choose_word()
     assert word in WORDS
+
+    def test_process_guess_correct():
+        game = initialize_game("coffee")
+        result = process_guess(game, "o")
+
+        assert result is True
+        assert game["display"] == ["_", "o", "_", "_", "_", "_"]
+        assert game["attempts_left"] == 6
