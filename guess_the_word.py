fe452245386a58ec9e043d8c7b1265c81543853d
Joffrey <jaydru@gmail.com>
2025-11-16 19:24:00 -0700

    Added play game() to check if letter is in word and print messages confirming or denying it was found

--- guess_the_word.py
+++ guess_the_word.py
@@ -33,3 +33,32 @@ def process_guess(game, letter):
         game["wrong_letters"].add(letter)
         game["attempts_left"] -= 1
         return False
+
+def play_game():
+    word = choose_word()
+    game = initialize_game(word)
+
+    print("Welcome to Guess the Word! Let's get started.")
+    print("I'm thinking of a word with", len(word), "letters.")
+    print("You have", game["attempts_left"], "wrong attempts allowed.\n")
+
+    while game["attempts_left"] > 0 and "_" in game["display"]:
+        print("Current word:", " ".join(game["display"]))
+        print("Wrong letters so far:", ", ".join(sorted(game["wrong_letters"])) or "None")
+        guess = input("Guess a letter: ")
+
+        if not guess.isalpha() or len(guess) != 1:
+            print("Please guess a single letter.\n")
+            continue
+
+        correct = process_guess(game, guess)
+
+        if correct:
+            print("Nice! That letter is in the word. Are you psychic?\n")
+        else:
+            print("Nope! That letter is NOT in the word. Try again.\n")
+
+    if "_" not in game["display"]:
+        print("Congratulations! You guessed the word:", word)
+    else:
+        print("Game over! The word was:", word)
